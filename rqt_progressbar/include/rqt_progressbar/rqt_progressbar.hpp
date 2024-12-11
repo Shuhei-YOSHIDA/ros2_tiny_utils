323fe1f19f802930e985247eb60959c24abf4f88
Shuhei-YOSHIDA <yoshida0525shu@gmail.com>
2024-12-11 23:17:05 +0900

    WIP: rqt_progressbar

--- rqt_progressbar/include/rqt_progressbar/rqt_progressbar.hpp
+++ rqt_progressbar/include/rqt_progressbar/rqt_progressbar.hpp
@@ -10,6 +10,24 @@
 namespace rqt_progressbar
 {
 
+class RqtProgressbar
+  : public rqt_gui_cpp::Plugin
+{
+  Q_OBJECT
+
+public:
+  RqtProgressbar();
+
+  virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+  virtual void shutdownPlugin();
+  virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings,
+      qt_gui_cpp::Settings& instance_settings) const;
+  virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings,
+      const qt_gui_cpp::Settings& instance_settings);
+
+
+};
+
 } // namespace rqt_progressbar
 
 #endif /* ifndef INCLUDE_RQT_PROGRESSBAR_RQT_PROGRESSBAR_HPP */
--- rqt_progressbar/src/rqt_progressbar/rqt_progressbar.cpp
+++ rqt_progressbar/src/rqt_progressbar/rqt_progressbar.cpp
@@ -3,3 +3,41 @@
  */
 
 #include "rqt_progressbar/rqt_progressbar.hpp"
+#include <pluginlib/class_list_macros.hpp>
+
+namespace rqt_progressbar
+{
+
+RqtProgressbar::RqtProgressbar()
+  : rqt_gui_cpp::Plugin()
+{
+  setObjectName("Rqt_test");
+}
+
+void RqtProgressbarinitPlugin(qt_gui_cpp::PluginContext& context)
+{
+
+}
+
+void RqtProgressbar::shutdownPlugin()
+{
+
+}
+
+void RqtProgressbar::saveSettings(
+    qt_gui_cpp::Settings& plugin_settings,
+    qt_gui_cpp::Settings& instance_settings) const
+{
+
+}
+
+void RqtProgressbar::restoreSettings(
+    const qt_gui_cpp::Settings& plugin_settings,
+    const qt_gui_cpp::Settings& instance_settings)
+{
+
+}
+
+} // namespace rqt_progressbar
+
+PLUGINLIB_EXPORT_CLASS(rqt_progressbar::RqtProgressbar, rqt_gui_cpp::Plugin)
